faf6bbaa6d810a4f2f2baa001f8c90c7f578aa8b
moyaqoob <moyaqoob28@gmail.com>
2025-02-06 21:13:40 +0530

    init

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import About from "./components/About";
-import Hero from "./components/Hero";
-import NavBar from "./components/Navbar";
-import Features from "./components/Features";
-import Story from "./components/Story";
 import Contact from "./components/Contact";
+import Features from "./components/Features";
 import Footer from "./components/Footer";
+import NavBar from "./components/Navbar";
+import Story from "./components/Story";
+import Hero from "./Components/Hero";
 
 function App() {
   return (
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,11 +1,9 @@
-import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
 import { ScrollTrigger } from "gsap/all";
-import { TiLocationArrow } from "react-icons/ti";
 import { useEffect, useRef, useState } from "react";
 
-import Button from "./Button";
-import VideoPreview from "./VideoPreview";
+import VideoPreview from "./components/VideoPreview";
 
 gsap.registerPlugin(ScrollTrigger);
 
@@ -84,17 +82,6 @@ const Hero = () => {
 
   return (
     <div className="relative h-dvh w-screen overflow-x-hidden">
-      {loading && (
-        <div className="flex-center absolute z-[100] h-dvh w-screen overflow-hidden bg-violet-50">
-          {/* https://uiverse.io/G4b413l/tidy-walrus-92 */}
-          <div className="three-body">
-            <div className="three-body__dot"></div>
-            <div className="three-body__dot"></div>
-            <div className="three-body__dot"></div>
-          </div>
-        </div>
-      )}
-
       <div
         id="video-frame"
         className="relative z-10 h-dvh w-screen overflow-hidden rounded-lg bg-blue-75"
@@ -139,34 +126,7 @@ const Hero = () => {
             onLoadedData={handleVideoLoad}
           />
         </div>
-
-        <h1 className="special-font hero-heading absolute bottom-5 right-5 z-40 text-blue-75">
-          G<b>A</b>MING
-        </h1>
-
-        <div className="absolute left-0 top-0 z-40 size-full">
-          <div className="mt-24 px-5 sm:px-10">
-            <h1 className="special-font hero-heading text-blue-100">
-              redefi<b>n</b>e
-            </h1>
-
-            <p className="mb-5 max-w-64 font-robert-regular text-blue-100">
-              Enter the Metagame Layer <br /> Unleash the Play Economy
-            </p>
-
-            <Button
-              id="watch-trailer"
-              title="Watch trailer"
-              leftIcon={<TiLocationArrow />}
-              containerClass="bg-yellow-300 flex-center gap-1"
-            />
-          </div>
-        </div>
       </div>
-
-      <h1 className="special-font hero-heading absolute bottom-5 right-5 text-black">
-        G<b>A</b>MING
-      </h1>
     </div>
   );
 };
