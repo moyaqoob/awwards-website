1658b79f6d5912bd45893ae1e694e257aa2b27c3
moyaqoob <moyaqoob28@gmail.com>
2025-02-15 19:14:10 +0530

    add

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -5,16 +5,20 @@ const Features = () => {
 
     const BentoCard = ({src,title,description})=>{
         return(
-            <div className='relative w-full'>
+            <div className='relative w-full  '>
                 <video src={src}
                 autoPlay
-                className='absolute left-0 top-0  object-cover object-center'
+                className='absolute left-0 top-0 object-cover object-center'
                 loop 
                 muted
                 ></video>
-                <div className='absolute text-4xl uppercase ml-9 font-circular-web mt-4'>
-                    <h1>{title}</h1>
+
+                <div className='absolute ml-9 font-circular-web mt-4 text-white'>
+                    <h1 className='text-4xl uppercase'>{title}</h1>
+                    <p className='mt-2 max-w-sm'>{description} </p>
                 </div>
+
+                
 
             </div>
 
@@ -24,17 +28,41 @@ const Features = () => {
     
    
   return (
-    <section className='bg-black pb-52 text-white'>
-            <div className='container mx-auto' >
-        `       <div className='px-5 py-10' >
+    <section className='bg-black pb-52 px-20 text-white'>
+            <div className='container mx-auto md-h[65vh]' >
+               <div className='px-5 py-10' >
                 <p>Explore the Zentry Universe</p>
                 <p className='max-w-md font-circular-web text-lg text-blue-50'>Immerse yourself in an IP-rich porudct universe where <br/> 
                     AI-driven gamification and hyper-personalization lead <br/>
                     the human-agentic economy.
                 </p>
                 </div>
-                <div className='border-hsla relative mb-7 h-96 w-full overflow-hidden rounded-md md:h-[65vh] '>
-                   <BentoCard src={"videos/feature-1.mp4"} title={<>radi<b>n</b>t</>} description={"A cross platform metagame app, turning your activities across Web2 and Web3 games into a rewarding adventure."} />
+                <div className='border-hsla relative mb-7 h-96 w-full overflow-hidden rounded-md '>
+                   <BentoCard src={"videos/feature-1.mp4"} title={<>radi<b>a</b>nt</>} description={"A cross platform metagame app, turning your activities across Web2 and Web3 games into a rewarding adventure."} />
+                </div>
+                <div className='grid h-[135vh] grid-rows-3  grid-cols-2 gap-7'>
+                    <div className='bento-tilt_1 row-span-1 md:row-span-2 md:col-span-1'>
+
+                        <BentoCard src={"videos/feature-2.mp4"}  title={<>ZIG <b>M</b>A</>} description={"The NFT collection merging Zentry's IP,AI, and gaming- pushing the boundaries of NFT innovation"} />
+                    </div>
+                    <div className='bento-tilt_1 row-span-1 ms-32 md:col-span-1 md:ms-0 '>
+
+                        <BentoCard  src={"videos/feature-3.mp4"}  title={<>N<b>E</b>XUS</>} description={"The player portal uniting humans & AI to play, compete,earn and showcase- gamifying social & Web3 experiences"} />
+                    </div>
+                    <div className='bento-tilt_1 me-14 md:col-span-1 md:me-0'>
+                         <BentoCard  src={"videos/feature-4.mp4"} title={<>AZ<b>U</b>L</>} description={"The agent of agents elevation agentic AI experience to be more fun and productive"} />
+
+                    </div>
+                    <div className='bento-tilt_2'>
+                            <div className='bg-blue-700 flex size-full flex-col justify-between p-5'>
+                                  <div className='bento-title max-w-[10px]'>
+                                        <h1>MORE  COMING SOON</h1>
+                                    </div>  
+                                    <div className=''>
+                                        <img src="img/play.svg" alt=""/>
+                                    </div>
+                            </div>
+                    </div>
                 </div>
 
              </div>
