2d9db8ff3f3ba8d036f943cc9a5d67c8fe18dd3e
moyaqoob <moyaqoob28@gmail.com>
2025-02-19 03:24:25 +0800

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-
-import Contact from "./Components/Contact";
+import React from "react";
 import Features from "./Components/Features";
 import Hero from "./Components/Hero";
 import Navbar from "./Components/Navbar";
 import Section from "./Components/section";
 import Story from "./Components/Story";
 import Footer from "./Components/Footer";
+import Contact from "./Components/Contact"
 
 function App() {
   return (
