631bb2ccab22c0d491c8618542297483d2f36878
moyaqoob <moyaqoob28@gmail.com>
2025-02-19 04:19:26 +0800

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import Features from './Components/Features.jsx';
-import Hero from './Components/Hero.jsx';
-import Navbar from './Components/Navbar.jsx';
-import Section from './Components/Section.jsx';
-import Story from './Components/Story.jsx';
-import Footer from './Components/Footer.jsx';
-import Contact from './Components/Contact.jsx'
+import Features from '@/Components/Features.jsx';
+import Hero from '@/Components/Hero.jsx';
+import Navbar from '@/Components/Navbar.jsx';
+import Section from '@/Components/Section.jsx';
+import Story from '@/Components/Story.jsx';
+import Footer from '@/Components/Footer.jsx';
+import Contact from '@/Components/Contact.jsx'
 
 function App() {
   return (
