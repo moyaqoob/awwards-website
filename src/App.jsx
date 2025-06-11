da3788bc7a4b093a9a1ad2a5165f7b0e77b0acf9
moyaqoob <moyaqoob28@gmail.com>
2025-06-11 20:34:20 +0530

    Add

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,22 @@
 // @ts-nocheck
-import Navbar from './Components/Navbar';
-import Hero from './Components/Hero';
-import Section from './Components/Section';
-import Features from './Components/Features';
-import Story from './Components/Story';
-import Contact from './Components/Contact';
-import Footer from './Components/Footer';
+import Contact from "./components/Contact";
+import Features from "./components/Features";
+import Footer from "./components/Footer";
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
+import Section from "./components/Section";
+import Story from "./components/Story";
 
 function App() {
   return (
     <main className="relative min-h-screen w-screen overflow-x-hidden">
-      <Navbar/>
+      <Navbar />
       <Hero />
-      <Section/>
-      <Features/>
-      <Story/>
-      <Contact/>
-      <Footer/>
+      <Section />
+      <Features />
+      <Story />
+      <Contact />
+      <Footer />
     </main>
   );
 }
